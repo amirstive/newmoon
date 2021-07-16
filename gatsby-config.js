89604cade324696d7fd28412f54c5598ecd4a804
amirstive <am_khaksar@yahoo.com>
2021-07-16 19:59:16 +0430

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,13 +29,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
-    {
-    resolve: 'gatsby-plugin-express',
-    options: {
-      output: 'config/gatsby-express.json',
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
